d82f8d0e8ed51dd6a485f1507eaa10daaf1cb4db
Marksu20 <markjoshuadlcrz@gmail.com>
2025-09-27 19:03:56 +0800

    add logout functionality to Navlinks component

--- src/components/Navlinks.jsx
+++ src/components/Navlinks.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import { RiChatAiFill, RiFolderUserFill, RiNotificationFill, RiFile4Fill, RiBardFill, RiShutDownLine, RiArrowDownSFill } from 'react-icons/ri'
+import { signOut } from 'firebase/auth'
+import { auth } from '../firebase/firebase'
 
 const Navlinks = () => {
   const iconStyle = { color: '#616367' }
+
+  const handleLogOut = async () => {
+    try {
+      await signOut(auth);
+    } catch (error) {
+      consoloe.log(error);
+    }
+  }
 
   return (
     <section className="sticky flex items-center lg:static lg:items-start lg:justify-start lg:h-[100vh] w-[100%] lg:w-[80px] lg:py-0 h-[7vh] top-2 py-8">{/* outline-1 outline-gray-400 */}
       <main className="flex lg:flex-col items-center lg:gap-10 justify-between lg:px-0 w-[100%]">
         {/* LOGO */}
-        <div className="flex items-start justify-center lg:border-b border-gray-300 lg:w-[100%] p-4 ml-3">
+        <div className="flex items-start justify-center  border-gray-300 lg:w-[100%] p-4 ml-3">
           <span className="flex items-center justify-center cursor-pointer">
             <img 
               src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" 
@@ -45,7 +55,10 @@ const Navlinks = () => {
             </button>
           </li>
           <li>
-            <button className="lg:text-[28px] text-[22px] cursor-pointer">
+            <button  
+              className="lg:text-[28px] text-[22px] cursor-pointer"
+              onClick={handleLogOut}
+            >
               <RiShutDownLine style={iconStyle} /> 
             </button>
           </li>
