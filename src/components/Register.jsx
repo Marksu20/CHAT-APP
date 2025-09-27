e37d427486f38c184ce28a6bf1c5b72928bff1a0
Marksu20 <markjoshuadlcrz@gmail.com>
2025-09-27 19:04:28 +0800

    implement user registration with Firebase authentication and update form handling

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import { useState } from 'react'
 import { FaUserPlus } from 'react-icons/fa'
 
-const Register = () => {
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth, db } from '../firebase/firebase';
+import { setDoc } from 'firebase/firestore';
+import { doc } from 'firebase/firestore';
+
+const Register = ({isLogin, setIsLogin}) => {
   const [userData, setUserData] = useState({
     fullName: "",
     email: "",
@@ -20,7 +25,18 @@ const Register = () => {
 
   const handleAuth = async () => {
     try {
-      alert("Register Successful" )
+      const userCredential = await createUserWithEmailAndPassword(auth, userData?.email, userData?.password);
+      const user = userCredential.user;
+
+      const userDocRef = doc(db, 'users', user.uid);
+
+      await setDoc(userDocRef, {
+        uid: user.uid,
+        email: user.email,
+        username: user.email?.split('@')[0],
+        fullName: userData.fullName,
+        image: '',
+      })
     } catch (error) {
       console.error(error)
     }
@@ -42,18 +58,17 @@ const Register = () => {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm outline-1 -outline-offset-1 outline-gray-100 p-8 rounded-lg shadow-lg">
-          <form action="#" method="POST" className="space-y-6">
+          <form method="POST" className="space-y-6">
             <div>
               <label htmlFor="fullName" className="block text-sm/6 font-medium text-gray-900">
                 Full Name
               </label>
               <div className="mt-2">
                 <input 
-                  id="" 
+                  id="fullname" 
                   name="fullName" 
                   type="text" 
                   required 
-                  autoComplete=""
                   onChange={handleChangeUserData} 
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
@@ -66,11 +81,10 @@ const Register = () => {
               </label>
               <div className="mt-2">
                 <input 
-                  id="" 
+                  id="email" 
                   name="email" 
                   type="email" 
                   required 
-                  autoComplete=""
                   onChange={handleChangeUserData} 
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
@@ -90,11 +104,10 @@ const Register = () => {
               </div>
               <div className="mt-2">
                 <input 
-                  id="" 
+                  id="password" 
                   name="password" 
                   type="password"
                   required 
-                  autoComplete="current-password"
                   onChange={handleChangeUserData} 
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
@@ -103,9 +116,9 @@ const Register = () => {
 
             <div>
               <button
-                type="submit"
+                type="button"
                 onClick={handleAuth}
-                className="flex justify-center items-center gap-2 w-full rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="flex justify-center items-center gap-2 w-full rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer"
               >
                 Sign up <FaUserPlus />
               </button>
@@ -115,9 +128,12 @@ const Register = () => {
         </div>
           <p className="mt-10 text-center text-sm/6 text-gray-500">
             Already have an account?{' '}
-            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+            <button 
+              className="font-semibold text-indigo-600 hover:text-indigo-500 cursor-pointer"
+              onClick={() => setIsLogin(!isLogin)}
+            >
               Sign in
-            </a>
+            </button>
           </p>
       </div>
     </>
