87901ebdc7dac3245429d00ab266ade7526ed145
Marksu20 <markjoshuadlcrz@gmail.com>
2025-09-27 19:03:07 +0800

    implement user authentication handling and conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import Chatbox from './components/Chatbox'
 import Chatlist from './components/Chatlist'
 import Login from './components/Login'
 import Navlinks from './components/Navlinks'
 import Register from './components/Register'
+import { auth } from './firebase/firebase'
 
 const App = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const currentUser = auth.currentUser;
+    if (currentUser) {
+      setUser(currentUser);
+    }
+
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+    
+    return () => unsubscribe();
+  }, [])
+
   return (
     <>
-      <div className="flex lg:flex-row flex-col w-full h-screen bg-[#F5F5F5]">
-        <Navlinks />
-        <Chatlist />
-        <Chatbox />
-      </div>
-      <div className="hidden">
-        <Login /> 
-        <Register />
-      </div>
+      {user ? (
+        <div className="flex lg:flex-row flex-col w-full h-screen bg-[#F5F5F5]">
+          <Navlinks />
+          <Chatlist />
+          <Chatbox />
+        </div>
+        ) : (
+        <div className="block">
+          { isLogin ? <Login isLogin={isLogin} setIsLogin={setIsLogin} /> : <Register isLogin={isLogin} setIsLogin={setIsLogin}  /> }
+        </div>
+        )
+      }
     </>
   )
 }
